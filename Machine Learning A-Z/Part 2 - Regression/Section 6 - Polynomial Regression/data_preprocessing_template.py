35046d458a1a2fa95ea3e8fe2bc8c3ae97adb015
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-10-09 13:25:17 +0200

    09102019

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/data_preprocessing_template.py
@@ -1,23 +1,29 @@
-# Data Preprocessing Template
-
-# Importing the libraries
+# import libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
-# Importing the dataset
+
+# import dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
+# creating matrix of features [lines:lines,columns:columns]
+X = dataset.iloc[:,:-1].values
+# creating dependent variable vector
 y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+# spliitting dataset into Training and Test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) # usually 0.2-0.3
+
+# stanadarisation: xst = x - mean(x)/st dev (x)
+# normalisation: xnorm = x - min(x)/max(x) - min(x)
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
+y_train = sc_y.fit_transform(y_train)"""
+
+
