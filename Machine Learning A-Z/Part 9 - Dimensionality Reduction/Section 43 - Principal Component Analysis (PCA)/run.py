4bf1cd461eed48269e0efe15d8e39c102c4ccc29
Ukabix <54454097+Ukabix@users.noreply.github.com>
2019-11-03 19:52:23 +0100

    03112019
    03112019

---
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/run.py
@@ -0,0 +1,97 @@
+# PCA
+
+# from m indep vars PCA extracts P=<n new indep vars that
+# explain the most the variance of the dataset, regardless of the DV.
+# Because DV is not considered -> PC is an unsupervised model
+
+# import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import dataset
+dataset = pd.read_csv('Wine.csv')
+
+
+## DATA PREPROCESSING
+
+# creating matrix of features
+X = dataset.iloc[:, 0:13].values 
+# creating dependent variable vector
+y = dataset.iloc[:, 13].values
+
+
+# splitting dataset into Training and Test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) # usually 0.2-0.3
+
+# stanadarisation: xst = x - mean(x)/st dev (x)
+# normalisation: xnorm = x - min(x)/max(x) - min(x)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+## END DATA PREPROCESSING
+
+
+# Applying PCA
+from sklearn.decomposition import PCA
+# call PCA obj - extracting 2 principle components
+pca = PCA(n_components = 2)
+X_train = pca.fit_transform(X_train, y_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+
+#### Fitting Logistic Regression to the Training Set
+# import libraries
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting Test Set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix - Evaluate if the model works well
+from sklearn.metrics import confusion_matrix # function
+cm = confusion_matrix(y_test, y_pred)
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.15, cmap = ListedColormap(('orange', 'blue', 'yellow')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = [ListedColormap(('orange', 'blue', 'yellow'))(i)], label = j)
+plt.title('LogReg (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.25, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = [ListedColormap(('red', 'green', 'blue'))(i)], label = j)
+plt.title('LogReg (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
